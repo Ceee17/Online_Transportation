e32703b1d18f57b2494c47b66c5da518235138cf
Ceee17 <jessen.535220023@stu.untar.ac.id>
2023-09-12 12:43:17 +0700

    Menambahkan Icon logo FlashMover

--- lib/pages/starting_page.dart
+++ lib/pages/starting_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
 
 class StartingPage extends StatelessWidget {
   const StartingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // backgroundColor: Color(0xFF3B60CE),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        // SizedBox(height: 50),
+        Center(
+          child: Image.asset(
+            'lib/assets/images/1.png',
+            width: 110,
+            height: 110,
+          ),
+        ),
+        // SizedBox(height: 100),
+        // Row(
+        //   children: [
+        //     Text(
+        //       'Fla',
+        //       style: TextStyle(
+        //         fontSize: 30,
+        //       ),
+        //     ),
+        //     Icon(
+        //       Icons.bolt,
+        //       size: 100,
+        //       color: Color(0xFFE5FF70),
+        //     ),
+        //     Text(
+        //       'hMover',
+        //       style: TextStyle(
+        //         fontSize: 30,
+        //       ),
+        //     )
+        //   ],
+        // )
+      ]),
+    );
   }
 }
