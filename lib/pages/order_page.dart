96938754e811c258a44dc0af447f3552da71935d
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2023-09-18 23:13:07 +0700

    perubahan pada order page dan penambahan CustomCardButton pada order page untuk menampilkan pesanan yang sedang berlangsung

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'templatehead.dart';
+import 'package:project_uts_online_transportation/pages/template/template-card-button.dart';
+import 'template/templatehead.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
@@ -9,54 +10,110 @@ class OrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
+        // disini menggunakan stack widget karena biar templatehead() tetep diatas, soalnya widget lainnya mw dibkin centered
         children: [
-          TemplateHead(
-            title: "ORDER",
+          Positioned(
+            //positioned widget cuma bisa dipake di dlm stack widget, mangknya harus pake stack
+            top: 0,
+            left: 0,
+            right: 0,
+            child: TemplateHead(
+              title: "ORDER",
+            ),
           ),
-          Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: 386,
-                height: 154,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: Colors.white),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    side: BorderSide(
-                      width: 2,
-                      color: Colors.red,
-                    ),
-                    elevation: 15,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25)),
-                  ),
-                  child: Row(
-                    children: [
-                      SvgPicture.asset(
-                        'lib/assets/images/flashride.svg',
-                        height: 100,
-                        width: 100,
-                      ),
-                      Text(
-                        'test',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
+          Center(
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // centered semua widgetnya
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-              )
-            ],
+                // backup()
+                CustomCardButton(
+                  svgAsset: 'lib/assets/images/flashride.svg',
+                  svgHeight: 80,
+                  title: 'FlashRide',
+                  description:
+                      'On The Way\nETA : 5 Mins\nPickup : Untar\nDestination : Mcdonald',
+                  color: 0xFF213A82,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomCardButton(
+                  svgAsset: 'lib/assets/images/flashcar.svg',
+                  svgHeight: 101,
+                  title: 'FlashCar',
+                  description:
+                      'On The Way\nETA : 5 Mins\nPickup : Untar\nDestination : Mcdonald',
+                  color: 0xFF3B60CE,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomCardButton(
+                    svgAsset: 'lib/assets/images/flashtaxi.svg',
+                    svgHeight: 80,
+                    title: 'FlashTaxi',
+                    description: 'test',
+                    color: 0xFF111D41)
+              ],
+            ),
           ),
         ],
+      ),
+    );
+  }
+
+  Container backup() {
+    return Container(
+      width: 386,
+      height: 154,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white,
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFF3B60CE),
+          side: BorderSide(
+            width: 2,
+            color: Colors.black,
+          ),
+          elevation: 15,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              'lib/assets/images/flashride.svg',
+              height: 80,
+              width: 80,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'FlashRide',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'On The Way\nETA : 5 Mins\nPickup : Untar\nDestination : Mcdonald',
+                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
