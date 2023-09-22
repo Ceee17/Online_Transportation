9703e5e36c7edc032c53adfc158f065365605ae9
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2023-09-22 17:47:52 +0700

    membuat Widget CustomNavBar(), dan memasukkannya ke orderpage

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -3,22 +3,26 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project_uts_online_transportation/pages/template/template-card-button.dart';
 import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
 import 'template/templatehead.dart';
+import 'template/template_nav_bar.dart';
 
-class OrderPage extends StatelessWidget {
+class OrderPage extends StatefulWidget {
+// pastiin classnya extend nya STATEFULWIDGET BUKAN STATELESS
   const OrderPage({super.key});
   static const String idScreen = "order";
+  @override
+  _OrderPageState createState() => _OrderPageState();
+  //buat navbar nya jg
+}
 
-  class _OrderPageState extends State<OrderPage> {
-    final Color navBarColor = Colors.red;
-    int selectedIndex = 0;
-    late PageController pageController;
-    @override
-    void initState(){
-      super.initState(){
-        super.initState();
-        pageController = PageController(initialPage: selectedIndex);
-      }
-    }
+class _OrderPageState extends State<OrderPage> {
+  int _selectedIndex = 2;
+  // index gw 2 karna order page urutan ke-3
+  // 0 - 1 - 2 - 3
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -42,7 +46,7 @@ class OrderPage extends StatelessWidget {
                   MainAxisAlignment.center, // centered semua widgetnya
               children: [
                 SizedBox(
-                  height: 20,
+                  height: 80,
                 ),
                 // backup()
                 CustomCardButton(
@@ -74,86 +78,92 @@ class OrderPage extends StatelessWidget {
                   description: 'test',
                   color: 0xFF111D41,
                 ),
-                WaterDropNavBar(
-                  backgroundColor: Colors.red,
-                  onItemSelected: (index) {
-                    setState(() {
-                      selectedIndex = index;
-                    });
-                    PageController.animateToPage(
-                      selectedIndex,
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.easeOutQuart,
-                    );
-                  },
-                  selectedIndex: selectedIndex,
-                  barItems: [
-                    BarItem(
-                      filledIcon: Icons.home_filled,
-                      outlinedIcon: Icons.home_max_outlined,
-                    ),
-                    BarItem(
-                      filledIcon: Icons.chat_bubble,
-                      outlinedIcon: Icons.chat_bubble_outlined,
-                    ),
-                  ],
-                )
               ],
             ),
           ),
         ],
       ),
-    );
-  }
-
-  Container backup() {
-    return Container(
-      width: 386,
-      height: 154,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        color: Colors.white,
-      ),
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Color(0xFF3B60CE),
-          side: BorderSide(
-            width: 2,
-            color: Colors.black,
-          ),
-          elevation: 15,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
-          ),
-        ),
-        child: Row(
-          children: [
-            SvgPicture.asset(
-              'lib/assets/images/flashride.svg',
-              height: 80,
-              width: 80,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'FlashRide',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  'On The Way\nETA : 5 Mins\nPickup : Untar\nDestination : Mcdonald',
-                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
+      // <BOTTOM-NAVBAR>
+      bottomNavigationBar: CustomNavBar(
+          selectedIndex: _selectedIndex, onItemSelected: _onItemTapped),
+      //</BOTTOM-NAVBAR>
     );
   }
 }
+
+  // WaterDropNavBar navBar() {
+  //   return WaterDropNavBar(
+  //     // index sesuain sama pages nya
+  //     selectedIndex: _selectedIndex,
+  //     onItemSelected: _onItemTapped,
+  //     barItems: [
+  //       BarItem(
+  //         filledIcon: Icons.home_filled,
+  //         outlinedIcon: Icons.home_outlined,
+  //       ),
+  //       BarItem(
+  //         filledIcon: Icons.forum,
+  //         outlinedIcon: Icons.forum_outlined,
+  //       ),
+  //       BarItem(
+  //           filledIcon: Icons.assignment,
+  //           outlinedIcon: Icons.assignment_outlined),
+  //       BarItem(
+  //         filledIcon: Icons.person_2,
+  //         outlinedIcon: Icons.person_2_outlined,
+  //       ),
+  //     ],
+  //     bottomPadding: 15.0,
+  //   );
+  // }
+
+  // Container backup() {
+  //   return Container(
+  //     width: 386,
+  //     height: 154,
+  //     decoration: BoxDecoration(
+  //       borderRadius: BorderRadius.circular(25),
+  //       color: Colors.white,
+  //     ),
+  //     child: ElevatedButton(
+  //       onPressed: () {},
+  //       style: ElevatedButton.styleFrom(
+  //         backgroundColor: Color(0xFF3B60CE),
+  //         side: BorderSide(
+  //           width: 2,
+  //           color: Colors.black,
+  //         ),
+  //         elevation: 15,
+  //         shape: RoundedRectangleBorder(
+  //           borderRadius: BorderRadius.circular(25),
+  //         ),
+  //       ),
+  //       child: Row(
+  //         children: [
+  //           SvgPicture.asset(
+  //             'lib/assets/images/flashride.svg',
+  //             height: 80,
+  //             width: 80,
+  //           ),
+  //           Column(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             crossAxisAlignment: CrossAxisAlignment.start,
+  //             children: [
+  //               Text(
+  //                 'FlashRide',
+  //                 style: TextStyle(
+  //                   fontSize: 20,
+  //                   fontWeight: FontWeight.w600,
+  //                 ),
+  //               ),
+  //               Text(
+  //                 'On The Way\nETA : 5 Mins\nPickup : Untar\nDestination : Mcdonald',
+  //                 style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
+  //               )
+  //             ],
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
---
+++ lib/pages/template/template_nav_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
+
+class CustomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onItemSelected;
+
+  CustomNavBar({
+    required this.selectedIndex,
+    required this.onItemSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return WaterDropNavBar(
+      selectedIndex: selectedIndex,
+      onItemSelected: onItemSelected,
+      barItems: [
+        BarItem(
+          filledIcon: Icons.home_filled,
+          outlinedIcon: Icons.home_outlined,
+        ),
+        BarItem(
+          filledIcon: Icons.forum,
+          outlinedIcon: Icons.forum_outlined,
+        ),
+        BarItem(
+          filledIcon: Icons.assignment,
+          outlinedIcon: Icons.assignment_outlined,
+        ),
+        BarItem(
+          filledIcon: Icons.person_2,
+          outlinedIcon: Icons.person_2_outlined,
+        ),
+      ],
+      bottomPadding: 15.0,
+    );
+  }
+}
