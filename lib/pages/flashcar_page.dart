0779bd1fb37eec5e2a7718b1a79c284e6a658d33
Shonnn23 <nelsoncarlss23@gmail.com>
2023-09-24 21:49:46 +0700

    menambahkan draggablescrollablescreensheet dan memperbarui flashcarpage serta menambahkan gambar gambar yang diperlukan

--- lib/pages/flashcar_page.dart
+++ lib/pages/flashcar_page.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:project_uts_online_transportation/pages/template/DraggableScrollableSheet.dart';
 
 class FlashCarPage extends StatelessWidget {
   const FlashCarPage({Key? key});
-  static const String idScreen = "flashcar";
+  static const String idScreen = 'flashcar';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(247.0), // Adjust the height as needed
+        preferredSize: Size.fromHeight(224.0),
         child: ClipRRect(
           borderRadius: BorderRadius.only(
             bottomLeft: Radius.circular(25.0),
@@ -18,30 +19,37 @@ class FlashCarPage extends StatelessWidget {
           child: Stack(
             children: <Widget>[
               AppBar(
-                title: Text('Halo cibai'),
                 backgroundColor: Color(0xff8DA2E2),
                 elevation: 0.0,
                 centerTitle: true,
               ),
               Positioned(
-                left: -28, // Adjusted to 0
-                top: 0,
-                bottom: 0,
+                top: 60,
+                left: -24,
                 child: Container(
-                  width: 200, // Adjust the width as needed
-                  child: Image.asset(
-                    'lib/assets/images/flashcarlogo.png',
+                  width: 170,
+                  child: Image.asset('lib/assets/images/flashcarlogo.png'),
+                ),
+              ),
+              Positioned(
+                top: 120,
+                left: -25,
+                child: Container(
+                  width: 230,
+                  child: SvgPicture.asset(
+                    'lib/assets/images/2cars.svg',
                   ),
                 ),
               ),
               Positioned(
-                left: 10, // Adjusted to 120
                 top: 120,
-                bottom: 0,
+                left: 190,
                 child: Container(
-                  width: 150, // Adjust the width as needed
-                  child: SvgPicture.asset(
-                    'lib/assets/images/2cars.svg',
+                  child: Text(
+                    'Book a FlashCar\nto get to places\nand send items\nconveniently',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
                   ),
                 ),
               ),
@@ -49,6 +57,24 @@ class FlashCarPage extends StatelessWidget {
           ),
         ),
       ),
+      body: Container(
+        padding: EdgeInsets.all(0.0),
+        child: Center(
+          child: Align(
+            alignment: Alignment(0.0, -0.9),
+            child: Text(
+              'Choose Flashride Service',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+      extendBody: true, // This allows the DragSheet to extend below the app bar
+      bottomNavigationBar:
+          DragSheet(), // Use bottomNavigationBar instead of bottomSheet
     );
   }
 }
