a6dc90ca901fffff2ba3c3c55ade92155d3c7773
Shonnn23 <nelsoncarlss23@gmail.com>
2023-09-26 15:42:28 +0700

    mengubah gambar location dan buat page pickupcar

--- lib/pages/flashcar_pickup.dart
+++ lib/pages/flashcar_pickup.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CarPickupPage extends StatelessWidget {
+class CarPickupPage extends StatefulWidget {
   const CarPickupPage({Key? key});
   static const String idScreen = 'carpickup';
 
+  @override
+  State<CarPickupPage> createState() => _CarPickupPageState();
+}
+
+class _CarPickupPageState extends State<CarPickupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +39,42 @@ class CarPickupPage extends StatelessWidget {
             ),
           ),
           Positioned(
+            top: 8,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Your Location',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: 350,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Pickup Location',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
             bottom: 0,
             left: 0,
             right: 0,
@@ -45,6 +86,194 @@ class CarPickupPage extends StatelessWidget {
                   topLeft: Radius.circular(34.0),
                   topRight: Radius.circular(34.0),
                 ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      // Add your onPressed logic here
+                    },
+                    child: Container(
+                      width: 360,
+                      height: 87,
+                      margin: EdgeInsets.only(bottom: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(36),
+                      ),
+                      child: Center(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal: 20,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment
+                                  .center, // Center text vertically
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Tarumanagara university',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  'Letjen S. Parman St no.1, RT.6/RW.16, Tomang, Grogol',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Add your onPressed logic here
+                    },
+                    child: Container(
+                      width: 370,
+                      height: 87,
+                      margin: EdgeInsets.only(bottom: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(36),
+                      ),
+                      child: Center(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal: 20,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment
+                                  .center, // Center text vertically
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Tarumanagara university',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  'Letjen S. Parman St no.1, RT.6/RW.16, Tomang, Grogol',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Add your onPressed logic here
+                    },
+                    child: Container(
+                      width: 370,
+                      height: 87,
+                      margin: EdgeInsets.only(bottom: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(36),
+                      ),
+                      child: Center(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal: 20,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment
+                                  .center, // Center text vertically
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Tarumanagara university',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  'Letjen S. Parman St no.1, RT.6/RW.16, Tomang, Grogol',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Add your onPressed logic here
+                    },
+                    child: Container(
+                      width: 332,
+                      height: 59,
+                      decoration: BoxDecoration(
+                        color: Color(0xff3B60CE),
+                        borderRadius: BorderRadius.circular(34),
+                      ),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Positioned(
+                            top:
+                                -10, // Adjust the top value to move the image down
+                            left:
+                                25, // Adjust the left value to move the image right
+                            child: Image.asset(
+                              'lib/assets/images/location1.png',
+                              width: 80,
+                              height: 80,
+                            ),
+                          ),
+                          Text(
+                            'Use Your Current Location',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 11,
+                              fontWeight: FontWeight.w100,
+                              decoration:
+                                  TextDecoration.underline, // Add this line
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
