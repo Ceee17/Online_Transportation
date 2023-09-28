12a95be630dfbf78052cf37d0039f73e08e88e4d
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2023-09-28 12:35:24 +0700

    benerin import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:project_uts_online_transportation/pages/landingpage.dart';
 import 'pages/loginpage.dart';
 import 'pages/loadingpage.dart';
 import 'package:project_uts_online_transportation/pages/account_page.dart';
