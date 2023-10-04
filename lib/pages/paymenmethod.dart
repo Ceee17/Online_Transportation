7f1922f819daeae9eb53125e4da40d9d613e3ae6
alfonsRichard <richardchrs.02@gmail.com>
2023-10-04 09:59:54 +0700

    menambahkan gambar box dan membuat paymen method lainnya

--- lib/pages/paymenmethod.dart
+++ lib/pages/paymenmethod.dart
@@ -26,19 +26,22 @@ class PaymenPage extends StatelessWidget {
                     child: Image.asset('lib/assets/images/FlashrideLogo.png'),
                   ),
                 ),
-                Text(
-                  'Your Payment Method',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  height: 80,
+                  child: Text(
+                    'Your Payment Method',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ],
             ),
-            Spacer(), // Add Spacer to fill remaining space
+            Spacer(),
             Container(
               width: double.infinity,
-              height: 612.4, // Set your desired fixed height here
+              height: 672.4,
               child: ClipRRect(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(25),
@@ -46,27 +49,55 @@ class PaymenPage extends StatelessWidget {
                 ),
                 child: Container(
                   color: Color(0xff8DA2E2),
-                  // child: ListView.builder(
-                  //   itemCount: 3,
-                  //   itemBuilder: (BuildContext context, int index) {
-                  //     return ListTile(
-                  //       title: Text('Item $index'),
-                  //     );
-                  //   },
-                  // ),
-
                   child: Column(
                     children: [
-                      SizedBox(
-                        height: 20,
+                      SizedBox(height: 20,),
+                      CustomCardButton(
+                        svgAsset: 'lib/assets/images/cash.svg',
+                        svgHeight: 80,
+                        svgWidth: 80,
+                        title: 'Flash Coins',
+                        description:
+                            'make payments \neasily and practically \nwithout hassle',
+                        color: Color(0xFF8DA2E2),
+                        textColor: Color(0xFF000000),
+                        heightButton: 140,
                       ),
+
+                      SizedBox(height: 20,),
                       CustomCardButton(
                         svgAsset: 'lib/assets/images/cash.svg',
                         svgHeight: 80,
                         svgWidth: 80,
                         title: 'Cash',
                         description:
-                            'On The Way\nETA : 5 Mins\nPickup : Untar\nDestination : Mcdonald',
+                            'make payments \ndirectly to drivers \nusing cash',
+                        color: Color(0xFF8DA2E2),
+                        textColor: Color(0xFF000000),
+                        heightButton: 140,
+                      ),
+
+                      SizedBox(height: 20,),
+                      CustomCardButton(
+                        svgAsset: 'lib/assets/images/cash.svg',
+                        svgHeight: 80,
+                        svgWidth: 80,
+                        title: 'Ovo',
+                        description:
+                            'make payments \ndirectly to drivers \nusing cash',
+                        color: Color(0xFF8DA2E2),
+                        textColor: Color(0xFF000000),
+                        heightButton: 140,
+                      ),
+
+                      SizedBox(height: 20,),
+                      CustomCardButton(
+                        svgAsset: 'lib/assets/images/cash.svg',
+                        svgHeight: 80,
+                        svgWidth: 80,
+                        title: 'Gopay',
+                        description:
+                            'make payments \ndirectly to drivers \nusing cash',
                         color: Color(0xFF8DA2E2),
                         textColor: Color(0xFF000000),
                         heightButton: 140,
