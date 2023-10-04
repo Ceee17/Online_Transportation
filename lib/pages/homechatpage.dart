9ceef214f7615a5cf037997b99b59ff60a6041e6
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2023-10-04 18:08:44 +0700

    Betulin posisi search bar, dan mmenambahkan route button pada account page, homechatpage, dan orderpage

--- lib/pages/homechatpage.dart
+++ lib/pages/homechatpage.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:project_uts_online_transportation/pages/account_page.dart';
 import 'package:project_uts_online_transportation/pages/chatpage.dart';
+import 'package:project_uts_online_transportation/pages/order_page.dart';
 import 'package:project_uts_online_transportation/pages/template/template_nav_bar.dart';
 
 class HomeChatPage extends StatefulWidget {
-  const HomeChatPage({Key? key});
+  const HomeChatPage({super.key});
 
   @override
   _HomeChatPageState createState() => _HomeChatPageState();
@@ -31,12 +33,10 @@ class _HomeChatPageState extends State<HomeChatPage> {
 
   late PageController _pageController;
   int _selectedIndex = 1;
-  // index gw 2 karna order page urutan ke-3
-  // 0 - 1 - 2 - 3
+
   @override
   void initState() {
     super.initState();
-    // Inisialisasi daftar kontak yang telah difilter dengan semua kontak awal.
     filteredContacts = contacts;
     _pageController = PageController(initialPage: _selectedIndex);
   }
@@ -44,13 +44,22 @@ class _HomeChatPageState extends State<HomeChatPage> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      // if (index == 3) {
-      //   Navigator.pushNamed(
-      //     context,
-      //     AccountPage.idScreen,
-      //   );
-      //   return;
-      // }
+      switch (index) {
+        case 2:
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => OrderPage(),
+            ),
+          );
+        case 3:
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AccountPage(),
+            ),
+          );
+      }
 
       _pageController.animateToPage(
         _selectedIndex,
@@ -73,121 +82,125 @@ class _HomeChatPageState extends State<HomeChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff213A82),
-      body: Column(
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
         children: [
-          Container(
-            // padding: EdgeInsets.only(
-            //   left: 20.0,
-            //   right: 20.0,
-            //   top: 50.0,
-            //   bottom: 20.0,
-
-            // ),
-            height: 85,
-            width: 420,
-            // color: Colors.black,
-            child: Stack(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Positioned(
-                  left: 40,
-                  bottom: 10,
-                  child: Text(
-                    "Chat",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                // Spacer(),
-                // SizedBox(width: 10),
-                Positioned(
-                  bottom: 10,
-                  left: 20,
-                  child: Image.asset(
-                    'lib/assets/images/undraw_Chat_re_re1u.png',
-                    width: 50,
-                    height: 50,
-                  ),
-                ),
-                Positioned(
-                  right: 10,
-                  bottom: 10,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 9,
-                        child: TextField(
-                          controller: _searchController,
-                          onChanged: (value) {
-                            setState(() {
-                              searchText = value;
-                            });
-                          },
-                          onEditingComplete: _searchContacts,
-                          decoration: InputDecoration(
-                            hintText: 'Search',
-                            hintStyle: TextStyle(color: Colors.white),
-                          ),
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      Positioned(
-                        top: 40,
-                        right: 10,
-                        child: Expanded(
-                          flex: 1,
-                          child: GestureDetector(
-                            onTap: _searchContacts,
-                            child: Container(
-                              padding: EdgeInsets.all(6),
-                              decoration: BoxDecoration(
-                                color: Color(0xff213A82),
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Icon(
-                                Icons.search,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          // Contact list
-          Expanded(
-            flex: 18,
-            child: SingleChildScrollView(
-              child: Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: filteredContacts
-                      .map((contact) => _buildContact(
-                            "lib/assets/images/undraw_Pic_profile_re_7g2h.png",
-                            contact,
-                            "Pesan Terbaru",
-                            "09.00",
-                          ))
-                      .toList(),
-                ),
-              ),
-            ),
-          ),
+          homeChatPageContent(),
         ],
       ),
       bottomNavigationBar: CustomNavBar(
         selectedIndex: _selectedIndex,
         onItemSelected: _onItemTapped,
       ),
+    );
+  }
+
+  Column homeChatPageContent() {
+    return Column(
+      children: [
+        Container(
+          height: 85,
+          width: 420,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 40,
+                bottom: 10,
+                child: Text(
+                  "Chat",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 10,
+                left: 20,
+                child: Image.asset(
+                  'lib/assets/images/undraw_Chat_re_re1u.png',
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 25,
+                  right: 2,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    // Expanded(
+                    //   flex: 19,
+                    Container(
+                      height: 50,
+                      width: 250,
+                      // color: Colors.black,
+                      child: TextField(
+                        controller: _searchController,
+                        onChanged: (value) {
+                          setState(() {
+                            searchText = value;
+                          });
+                        },
+                        onEditingComplete: _searchContacts,
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Positioned(
+                      top: 40,
+                      right: 10,
+                      child: GestureDetector(
+                        onTap: _searchContacts,
+                        child: Container(
+                          padding: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                            color: Color(0xff213A82),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 18,
+          child: SingleChildScrollView(
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: filteredContacts
+                    .map((contact) => _buildContact(
+                          "lib/assets/images/undraw_Pic_profile_re_7g2h.png",
+                          contact,
+                          "Pesan Terbaru",
+                          "09.00",
+                        ))
+                    .toList(),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -214,13 +227,12 @@ class _HomeChatPageState extends State<HomeChatPage> {
       ),
       trailing: Text(time),
       onTap: () {
-        // Navigate to the ChatScreen when a contact is tapped.
         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => ChatScreen(
               contactName: name,
-              contactImage: pngAsset, // Mengimpor profil kontak ke ChatScreen
+              contactImage: pngAsset,
             ),
           ),
         );
