9ca0471b558b483d1bfefd6bfc48efb8da5fd9fc
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2023-10-06 14:25:06 +0700

    menambah pubspec yaml, yaitu shareds preference, path, path provider, sqflite, untuk membuat orderpage berfungsi seperti "cart" agar dapat menampung orderan yang sedang aktif

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,32 @@
 // ignore_for_file: unused_import
 
 import 'package:flutter/material.dart';
+// import 'package:project_uts_online_transportation/pages/flashcar_item_page.dart';
+// import 'package:project_uts_online_transportation/pages/google_maps.dart';
+// import 'package:project_uts_online_transportation/pages/flashridedestination.dart';
+// import 'pages/flashpick_driverotw.dart';
+// import 'package:project_uts_online_transportation/pages/flashcar_destination.dart';
+// import 'package:project_uts_online_transportation/pages/flashride_pickup.dart';
+// import 'package:project_uts_online_transportation/pages/flashcar_item_page.dart';
+// import 'pages/flashride_confirmationpage.dart';
+// import 'pages/flashridedestination.dart';
+// import 'pages/flashride_mainpage.dart';
+
 import 'pages/chatpage.dart';
 import 'pages/faqpage.dart';
 import 'pages/homechatpage.dart';
 import 'pages/flashride_items_page.dart';
 import 'pages/flashtaxi.dart';
-
 import 'pages/loginpage.dart';
 import 'pages/loadingpage.dart';
 import 'package:project_uts_online_transportation/pages/account_page.dart';
-import 'package:project_uts_online_transportation/pages/destinationcar.dart';
 import 'package:project_uts_online_transportation/pages/flashcar_pickup.dart';
 import 'package:project_uts_online_transportation/pages/history_page.dart';
 import 'pages/startingpage.dart';
 import 'pages/flashcar_page.dart';
 import 'pages/order_page.dart';
-import 'pages/flashpick.dart';
 import 'pages/searchingdriver.dart';
-import 'pages/flashride_page.dart';
+
 import 'pages/moveperson_page.dart';
 import 'pages/signuppage.dart';
 import 'pages/forgotpasswordpage.dart';
